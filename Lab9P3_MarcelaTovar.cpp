e5edfaa8adf47a2fd4715ec38d9e6ed8b36da6cb
Marcela Tovar <marcelatovar.flores@gmail.com>
2023-06-16 13:23:45 -0600

    Menu y creacion de clase

--- Lab9P3_MarcelaTovar.cpp
+++ Lab9P3_MarcelaTovar.cpp
@@ -1,8 +1,41 @@
-
 #include <iostream>
-
+using namespace std;
+void menu() {
+    bool check = true;
+    int opcion = 0;
+    do {
+        cout << "---MENU---" << endl;
+        cout << "1. Crear cuenta de PayPal"<<endl;
+        cout << "2. Crear Wallet" << endl;
+        cout << "3. Cargar informacion" << endl;
+        cout << "4. Acceder a PayPal" << endl;
+        cout << "5. Acceder a Wallet" << endl;
+        cout << "6. Salir" << endl;
+        cout << "Que desea hacer?" << endl;
+        cin >> opcion;
+        switch (opcion) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            check = false;
+            break;
+        default:
+            cout << "Escoja una opcion valida" << endl;
+            break;
+        }
+    } while (check);
+    cout << "Bye bye! :D" << endl;
+}
 int main(){
-    std::cout << "Hello World!\n";
+    menu();
 }
 
 
---
+++ UsuarioPaypal.cpp
@@ -0,0 +1,1 @@
+#include "UsuarioPaypal.h"
